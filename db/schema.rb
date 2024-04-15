616188cea8ebab1aa631fbf578ea2563b5ee0948
EC2 Default User <164721692+HiromitsuMiyahara@users.noreply.github.com>
2024-04-15 07:11:58 +0000

    [Add] 3-16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_14_041559) do
+ActiveRecord::Schema.define(version: 2024_04_14_040815) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
